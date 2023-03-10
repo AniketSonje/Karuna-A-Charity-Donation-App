b513e0a48108ab8d7a009d9c4b17820c31e40549
AniketSonje <86477052+AniketSonje@users.noreply.github.com>
2023-03-10 13:13:20 +0530

    Add files via upload

---
+++ KarunaUpdated/src/pages/about/About.js
@@ -0,0 +1,24 @@
+import './About.css';
+const About=()=>{
+    return <div>
+        <h2>rendering about us page</h2>
+        
+        <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>We are a team of passionate individuals who are dedicated to providing high-quality products and services to our customers.</p>
+      <h2>Our Mission</h2>
+      <p>Our mission is to empower people to achieve their goals by providing them with the tools and resources they need to succeed.</p>
+      <h2>Our Values</h2>
+      <ul>
+        <li>Integrity</li>
+        <li>Customer satisfaction</li>
+        <li>Innovation</li>
+        <li>Collaboration</li>
+        <li>Continuous improvement</li>
+      </ul>
+    </div>
+
+
+    </div>
+}
+export default About;
